8ca3245ad70e102b04503a2dc17afbb2292e6362
emi-ran <emirhan551952@gmail.com>
2024-09-18 20:37:46 +0300

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(-3, -3)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(81, 94)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(-5, 89)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(84, 30)
+        Button1.TabIndex = 1
+        Button1.Text = "Check"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(76, 115)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        FormBorderStyle = FormBorderStyle.SizableToolWindow
+        Name = "Form1"
+        Text = "Kopya"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Collections.Generic
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ProcessText()
+    End Sub
+
+    Private Sub ProcessText()
+        Dim lines As String() = TextBox1.Lines
+        Dim uniqueLines As New List(Of String)
+        Dim duplicateCount As Integer = 0
+
+        For Each line As String In lines
+            If Not uniqueLines.Contains(line) Then
+                uniqueLines.Add(line)
+            Else
+                duplicateCount += 1
+            End If
+        Next
+
+        TextBox1.Lines = uniqueLines.ToArray()
+        Clipboard.SetText(String.Join(Environment.NewLine, uniqueLines))
+        MessageBox.Show($"Toplam {duplicateCount} aynı satır bulundu ve silindi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
